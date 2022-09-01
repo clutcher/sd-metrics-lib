28419725d7850a25a69f083b37011f808dc5cfb8
Igor Zarvanskyi <iclutcher@gmail.com>
2022-09-01 21:59:24 +0200

    Extend velocity calculator time units

--- src/calculators/velocity_calculator.py
+++ src/calculators/velocity_calculator.py
@@ -5,10 +5,17 @@ from data_providers import WorklogExtractor
 from data_providers.issue_provider import IssueProvider
 from data_providers.story_point_extractor import StoryPointExtractor
 
+SECONDS_IN_HOUR = 3600
+WORKING_HOURS_PER_DAY = 8
+WORKING_DAYS_PER_WEEK = 5
+WORKING_WEEKS_IN_MONTH = 4
+
 
 class VelocityTimeUnit(Enum):
-    DAY = auto()
     HOUR = auto()
+    DAY = auto()
+    WEEK = auto()
+    MONTH = auto()
 
 
 class UserVelocityCalculator:
@@ -66,9 +73,13 @@ class UserVelocityCalculator:
 
     @staticmethod
     def _convert_spent_time(spent_time_in_seconds: int, time_unit: VelocityTimeUnit):
-        if time_unit == VelocityTimeUnit.DAY:
-            return spent_time_in_seconds / 3600 / 8
-        elif time_unit == VelocityTimeUnit.HOUR:
-            return spent_time_in_seconds / 3600
+        if time_unit == VelocityTimeUnit.HOUR:
+            return spent_time_in_seconds / SECONDS_IN_HOUR
+        elif time_unit == VelocityTimeUnit.DAY:
+            return spent_time_in_seconds / SECONDS_IN_HOUR / WORKING_HOURS_PER_DAY
+        elif time_unit == VelocityTimeUnit.WEEK:
+            return spent_time_in_seconds / SECONDS_IN_HOUR / WORKING_HOURS_PER_DAY / WORKING_DAYS_PER_WEEK
+        elif time_unit == VelocityTimeUnit.MONTH:
+            return spent_time_in_seconds / SECONDS_IN_HOUR / WORKING_HOURS_PER_DAY / WORKING_DAYS_PER_WEEK / WORKING_WEEKS_IN_MONTH
         else:
             return spent_time_in_seconds
