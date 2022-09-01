137570ff6a2a2c9bc8db5b6374c5841c0d03604e
Igor Zarvanskyi <iclutcher@gmail.com>
2022-09-01 23:37:53 +0200

    Add TimeRangeGenerator util class

--- src/data_providers/utils/__init__.py
+++ src/data_providers/utils/__init__.py
@@ -1,2 +1,2 @@
 from data_providers.utils.enums import VelocityTimeUnit
-from data_providers.utils.query_utils import JiraIssueSearchQueryBuilder
+from data_providers.utils.query_utils import JiraIssueSearchQueryBuilder, TimeRangeGenerator
