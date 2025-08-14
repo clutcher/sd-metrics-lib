62f54426c7298341e70bae16e15615a1fa012d48
Igor Zarvanskyi <iclutcher@gmail.com>
2025-08-14 19:32:22 +0200

    feat: extend JiraSearchQueryBuilder to support open ended resolution date tuple

--- src/data_providers/jira/query_builder.py
+++ src/data_providers/jira/query_builder.py
@@ -1,5 +1,6 @@
 import datetime
 from enum import Enum, auto
+from typing import Optional
 
 
 class JiraSearchQueryBuilder:
@@ -12,7 +13,7 @@ class JiraSearchQueryBuilder:
 
     def __init__(self,
                  projects: list[str] = None,
-                 resolution_dates: tuple[datetime.datetime, datetime.datetime] = None,
+                 resolution_dates: tuple[Optional[datetime.datetime], Optional[datetime.datetime]] = None,
                  statuses: list[str] = None,
                  task_types: list[str] = None
                  ) -> None:
@@ -35,21 +36,23 @@ class JiraSearchQueryBuilder:
         status_filter = "status in (" + self.__convert_in_jql_value_list(statuses) + ")"
         self.__add_filter(self.__QueryParts.STATUS, status_filter)
 
-    def for_resolution_dates(self, resolution_dates: tuple[datetime.datetime, datetime.datetime]):
+    def for_resolution_dates(self, resolution_dates: tuple[Optional[datetime.datetime], Optional[datetime.datetime]]):
         if resolution_dates is None:
             return
         date_filter = self.__create_date_range_filter("resolutiondate",
                                                       resolution_dates[0],
                                                       resolution_dates[1])
-        self.__add_filter(self.__QueryParts.RESOLUTION_DATE, date_filter)
+        if date_filter:
+            self.__add_filter(self.__QueryParts.RESOLUTION_DATE, date_filter)
 
-    def for_last_modified_dates(self, last_modified_datas: tuple[datetime.datetime, datetime.datetime]):
+    def for_last_modified_dates(self, last_modified_datas: tuple[Optional[datetime.datetime], Optional[datetime.datetime]]):
         if last_modified_datas is None:
             return
         date_filter = self.__create_date_range_filter("updated",
                                                       last_modified_datas[0],
                                                       last_modified_datas[1])
-        self.__add_filter(self.__QueryParts.LAST_MODIFIED, date_filter)
+        if date_filter:
+            self.__add_filter(self.__QueryParts.LAST_MODIFIED, date_filter)
 
     def for_task_types(self, task_types):
         if task_types is None:
@@ -65,10 +68,15 @@ class JiraSearchQueryBuilder:
         return ', '.join(['"%s"' % w for w in statuses])
 
     @staticmethod
-    def __create_date_range_filter(field_name: str, start_date: datetime.date, end_date: datetime.date):
-        start_date_str = start_date.strftime('%Y-%m-%d')
-        end_date_str = end_date.strftime('%Y-%m-%d')
-        return "%s >= '%s' and %s <= '%s'" % (field_name, start_date_str, field_name, end_date_str)
+    def __create_date_range_filter(field_name: str, start_date: Optional[datetime.date], end_date: Optional[datetime.date]):
+        parts = []
+        if start_date is not None:
+            start_date_str = start_date.strftime('%Y-%m-%d')
+            parts.append(f"{field_name} >= '{start_date_str}'")
+        if end_date is not None:
+            end_date_str = end_date.strftime('%Y-%m-%d')
+            parts.append(f"{field_name} <= '{end_date_str}'")
+        return ' and '.join(parts)
 
     def __add_filter(self, query_part_type: __QueryParts, query_part):
         self.query_parts[query_part_type] = query_part.strip()
