4a6aa4de921be58030291a5937a5a5e146156bb0
Igor Zarvanskyi <iclutcher@gmail.com>
2025-08-18 01:03:45 +0200

    feat: fetch all fields for subtasks in JiraTaskProvider

--- sd_metrics_lib/sources/jira/tasks.py
+++ sd_metrics_lib/sources/jira/tasks.py
@@ -24,17 +24,22 @@ class JiraTaskProvider(TaskProvider):
         self.thread_pool_executor = thread_pool_executor
 
     def get_tasks(self):
-        first_page = self.jira_client.jql(self.query, expand=self._expand_str, limit=self._get_task_fetch_amount())
+        tasks = self._fetch_tasks(self.query, self._expand_str)
+        if self.additional_fields and 'subtasks' in self.additional_fields:
+            self._fetch_child_tasks_and_replace_subtasks_field(tasks)
+
+        return tasks
+
+    def _fetch_tasks(self, query: str, expand_str: str):
+        first_page = self.jira_client.jql(query, expand=expand_str, limit=self._get_task_fetch_amount())
         first_page_tasks = first_page.get("issues", [])
         tasks_total_count = first_page.get("total", len(first_page_tasks))
         page_len = len(first_page_tasks)
-
         if tasks_total_count == 0 or page_len == 0:
             return []
 
         tasks = []
         tasks.extend(first_page_tasks)
-
         if page_len < tasks_total_count:
             amount_of_fetches = math.ceil(tasks_total_count / float(page_len))
 
@@ -42,7 +47,6 @@ class JiraTaskProvider(TaskProvider):
                 self._fetch_task_sync(tasks, amount_of_fetches, page_len)
             else:
                 self._fetch_task_concurrently(tasks, amount_of_fetches, page_len)
-
         return tasks
 
     def _fetch_task_concurrently(self, tasks, amount_of_fetches, page_len):
@@ -68,6 +72,49 @@ class JiraTaskProvider(TaskProvider):
                                                        start=start)
             current_page_tasks = current_page_result.get("issues", [])
             tasks.extend(current_page_tasks)
+
+    def _fetch_child_tasks_and_replace_subtasks_field(self, jira_tasks: Iterable[dict]):
+        if not jira_tasks:
+            return
+
+        child_tasks_ids = []
+        task_id_to_child_tasks_ids = {}
+        for jira_task in jira_tasks:
+            subtasks = jira_task.get('fields', {}).get('subtasks', [])
+            if subtasks:
+                subtasks_ids = [subtask.get('key') for subtask in subtasks if subtask.get('key')]
+                if subtasks_ids:
+                    child_tasks_ids.extend(subtasks_ids)
+                    task_id_to_child_tasks_ids[jira_task['key']] = subtasks_ids
+
+        if not child_tasks_ids:
+            return
+
+        child_task_id_to_child_task = self._fetch_tasks_by_id(child_tasks_ids)
+        for task in jira_tasks:
+            task_key = task['key']
+            if task_key in task_id_to_child_tasks_ids:
+                task['fields']['subtasks'] = self._create_child_task_list(
+                    task_key,
+                    task_id_to_child_tasks_ids,
+                    child_task_id_to_child_task
+                )
+
+    def _fetch_tasks_by_id(self, task_ids):
+        query = "key in (" + ", ".join(task_ids) + ")"
+        child_tasks = self._fetch_tasks(
+            query,
+            ",".join([field for field in self.additional_fields if field != 'subtasks'])
+        )
+        return {task['key']: task for task in child_tasks}
+
+    @staticmethod
+    def _create_child_task_list(task_key, task_to_child_tasks_ids, child_task_id_to_child_task):
+        return [
+            child_task_id_to_child_task[child_key]
+            for child_key in task_to_child_tasks_ids[task_key]
+            if child_key in child_task_id_to_child_task
+        ]
 
     def _get_task_fetch_amount(self):
         if self.thread_pool_executor is None:
