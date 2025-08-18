68630a6a2b22c5b8e6ae32486ad0ae96fb786dd6
Igor Zarvanskyi <iclutcher@gmail.com>
2025-08-18 18:32:58 +0200

    feat: add possibility to convert time using non standard amount of working hours per day

--- sd_metrics_lib/utils/time.py
+++ sd_metrics_lib/utils/time.py
@@ -13,14 +13,18 @@ def get_seconds_in_day() -> int:
     return WORKING_HOURS_PER_DAY * SECONDS_IN_HOUR
 
 
-def convert_time(spent_time_in_seconds: int, time_unit: VelocityTimeUnit):
+def convert_time(
+        spent_time_in_seconds: int,
+        time_unit: VelocityTimeUnit,
+        ideal_working_hours_per_day: int = WORKING_HOURS_PER_DAY
+) -> float:
     if spent_time_in_seconds is None:
         return 0
     if time_unit == VelocityTimeUnit.HOUR:
         return spent_time_in_seconds / SECONDS_IN_HOUR
     elif time_unit == VelocityTimeUnit.DAY:
-        return spent_time_in_seconds / SECONDS_IN_HOUR / WORKING_HOURS_PER_DAY
+        return spent_time_in_seconds / SECONDS_IN_HOUR / ideal_working_hours_per_day
     elif time_unit == VelocityTimeUnit.WEEK:
-        return spent_time_in_seconds / SECONDS_IN_HOUR / WORKING_HOURS_PER_DAY / WORKING_DAYS_PER_WEEK
+        return spent_time_in_seconds / SECONDS_IN_HOUR / ideal_working_hours_per_day / WORKING_DAYS_PER_WEEK
     elif time_unit == VelocityTimeUnit.MONTH:
-        return spent_time_in_seconds / SECONDS_IN_HOUR / WORKING_HOURS_PER_DAY / WORKING_DAYS_PER_WEEK / WORKING_WEEKS_IN_MONTH
+        return spent_time_in_seconds / SECONDS_IN_HOUR / ideal_working_hours_per_day / WORKING_DAYS_PER_WEEK / WORKING_WEEKS_IN_MONTH
